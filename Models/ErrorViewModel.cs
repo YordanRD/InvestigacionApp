c08996e9a848b04128f42b7cc715043ec8ce822f
Yordan <yordanrd.u@gmail.com>
2020-07-24 20:56:53 -0600

    Aplicacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppInvestigacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
